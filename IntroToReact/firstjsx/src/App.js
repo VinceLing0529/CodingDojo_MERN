89f7b7d948b6b4eb355d73528a3554f05d182569
VinceLing0529 <ldkxnb@gmail.com>
2021-07-07 22:54:34 -0700

    firstjsx

---
+++ IntroToReact/firstjsx/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hell Dojo!</h1>
+      <h2>Thinghs I need to do</h2>
+      <ul>
+        <li>Learn React</li>
+        <li>Climb mt eversest</li>
+        <li>run a marathon</li>
+        <li>feed the dogs</li>
+      </ul>
+
+
+    </div>
+  );
+}
+
+export default App;
